3ef6bede3d953f6401148a858a2cf5193fd66933
muhammadhaikalfauzananda <muhammad.120140168@student.itera.ac.id>
2023-04-17 23:50:08 +0700

    tugas 4
    tugas 4

---
+++ TugasIndividu4_Muhammad Haikal Fauzananda_120140168/page/add.js
@@ -0,0 +1,17 @@
+import { View } from "react-native-web";
+
+const add = () => {
+    return
+        <View>
+            <View>
+                <TextInput>Masukan ID</TextInput>
+                <TextInput>Asal</TextInput>
+                <TextInput>Tujuan</TextInput>
+                <TextInput>Maskapai</TextInput>
+                <TextInput>Waktu</TextInput>
+            </View>
+
+        </View>
+}
+
+export default add;
