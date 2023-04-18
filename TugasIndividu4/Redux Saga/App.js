f92b87fd24b1b30b7b9b46553f93cc145901b7d1
muhammadhaikalfauzananda <muhammad.120140168@student.itera.ac.id>
2023-04-18 14:50:30 +0700

    tugas4
    tugas4

---
+++ TugasIndividu4/Redux Saga/App.js
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Provider } from "react-redux";
+import store from "./page/Redux-Saga/store";
+import Home from './page/Home';
+import Penerbangan from './page/Penerbangan';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <Provider store={store}>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Penerbangan" component={Penerbangan} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  );
+}
+export default App;
