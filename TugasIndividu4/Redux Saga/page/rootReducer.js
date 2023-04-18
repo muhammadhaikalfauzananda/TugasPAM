f97d58c552c5c8dea2b9ca2d8364ffb1be99cdfc
muhammadhaikalfauzananda <100905990+muhammadhaikalfauzananda@users.noreply.github.com>
2023-04-18 17:33:22 +0700

    Delete TugasIndividu4 directory

--- TugasIndividu4/Redux Saga/page/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import dataIPReducer from "./redux-saga/Reducer";
-
-const rootReducer = combineReducers({
-  data: dataIPReducer,
-});
-
-export default rootReducer;
--- TugasIndividu4/Redux Thunk/page/redux-thunk/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore, applyMiddleware } from "redux";
-import thunk from "redux-thunk";
-import ipReducer from "./Reducer";
-
-const middleware = [thunk];
-
-const store = createStore(ipReducer, applyMiddleware(...middleware));
-
-export default store;
